4a5bd4dedb96c20c96f705deadb001ec32dc5f4d
aggabaorenz16 <43602006+aggabaorenz16@users.noreply.github.com>
2021-03-01 18:54:38 -0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,133 @@
+fetch('https://corona.lmao.ninja/v2/countries/Philippines')
+.then((response) => {
+    return response.json();
+})
+.then((data)=>{
+document.getElementById('ph-Actived').innerHTML = data.active;
+document.getElementById('ph-deaths').innerHTML = data.deaths;
+document.getElementById('ph-recovered').innerHTML = data.recovered;
+document.getElementById('ph-Case').innerHTML = data.cases;
+document.getElementById('ph-tdcases').innerHTML = data.todayCases;
+})
+fetch('https://corona.lmao.ninja/v2/countries/Singapore')
+.then((response) => {
+    return response.json();
+})
+.then((data)=>{
+document.getElementById('sg-Actived').innerHTML = data.active;
+document.getElementById('sg-deaths').innerHTML = data.deaths;
+document.getElementById('sg-recovered').innerHTML = data.recovered;
+document.getElementById('sg-Case').innerHTML = data.cases;
+document.getElementById('sg-tdcases').innerHTML = data.todayCases;
+})
+fetch('https://corona.lmao.ninja/v2/countries/Vietnam')
+.then((response) => {
+    return response.json();
+})
+.then((data)=>{
+document.getElementById('vn-Actived').innerHTML = data.active;
+document.getElementById('vn-deaths').innerHTML = data.deaths;
+document.getElementById('vn-recovered').innerHTML = data.recovered;
+document.getElementById('vn-Case').innerHTML = data.cases;
+document.getElementById('vn-tdcases').innerHTML = data.todayCases;
+})
+fetch('https://corona.lmao.ninja/v2/countries/Japan')
+.then((response) => {
+    return response.json();
+})
+.then((data)=>{
+document.getElementById('jp-Actived').innerHTML = data.active;
+document.getElementById('jp-deaths').innerHTML = data.deaths;
+document.getElementById('jp-recovered').innerHTML = data.recovered;
+document.getElementById('jp-Case').innerHTML = data.cases;
+document.getElementById('jp-tdcases').innerHTML = data.todayCases;
+})
+fetch('https://corona.lmao.ninja/v2/countries/Kuwait')
+.then((response) => {
+    return response.json();
+})
+.then((data)=>{
+document.getElementById('kw-Actived').innerHTML = data.active;
+document.getElementById('kw-deaths').innerHTML = data.deaths;
+document.getElementById('kw-recovered').innerHTML = data.recovered;
+document.getElementById('kw-Case').innerHTML = data.cases;
+document.getElementById('kw-tdcases').innerHTML = data.todayCases;
+})
+fetch('https://corona.lmao.ninja/v2/countries/Kuwait')
+.then((response) => {
+    return response.json();
+})
+.then((data)=>{
+document.getElementById('bh-Actived').innerHTML = data.active;
+document.getElementById('bh-deaths').innerHTML = data.deaths;
+document.getElementById('bh-recovered').innerHTML = data.recovered;
+document.getElementById('bh-Case').innerHTML = data.cases;
+document.getElementById('bh-tdcases').innerHTML = data.todayCases;
+})
+fetch('https://corona.lmao.ninja/v2/countries/China')
+.then((response) => {
+    return response.json();
+})
+.then((data)=>{
+document.getElementById('cn-Actived').innerHTML = data.active;
+document.getElementById('cn-deaths').innerHTML = data.deaths;
+document.getElementById('cn-recovered').innerHTML = data.recovered;
+document.getElementById('cn-Case').innerHTML = data.cases;
+document.getElementById('cn-tdcases').innerHTML = data.todayCases;
+})
+fetch('https://corona.lmao.ninja/v2/countries/Indonesia')
+.then((response) => {
+    return response.json();
+})
+.then((data)=>{
+document.getElementById('in-Actived').innerHTML = data.active;
+document.getElementById('in-deaths').innerHTML = data.deaths;
+document.getElementById('in-recovered').innerHTML = data.recovered;
+document.getElementById('in-Case').innerHTML = data.cases;
+document.getElementById('in-tdcases').innerHTML = data.todayCases;
+})
+fetch('https://corona.lmao.ninja/v2/countries/Thailand')
+.then((response) => {
+    return response.json();
+})
+.then((data)=>{
+document.getElementById('th-Actived').innerHTML = data.active;
+document.getElementById('th-deaths').innerHTML = data.deaths;
+document.getElementById('th-recovered').innerHTML = data.recovered;
+document.getElementById('th-Case').innerHTML = data.cases;
+document.getElementById('th-tdcases').innerHTML = data.todayCases;
+})
+fetch('https://corona.lmao.ninja/v2/countries/Taiwan')
+.then((response) => {
+    return response.json();
+})
+.then((data)=>{
+document.getElementById('tw-Actived').innerHTML = data.active;
+document.getElementById('tw-deaths').innerHTML = data.deaths;
+document.getElementById('tw-recovered').innerHTML = data.recovered;
+document.getElementById('tw-Case').innerHTML = data.cases;
+document.getElementById('tw-tdcases').innerHTML = data.todayCases;
+})
+fetch('https://corona.lmao.ninja/v2/countries/Pakistan')
+.then((response) => {
+    return response.json();
+})
+.then((data)=>{
+document.getElementById('pk-Actived').innerHTML = data.active;
+document.getElementById('pk-deaths').innerHTML = data.deaths;
+document.getElementById('pk-recovered').innerHTML = data.recovered;
+document.getElementById('pk-Case').innerHTML = data.cases;
+document.getElementById('pk-tdcases').innerHTML = data.todayCases;
+})
+fetch('https://corona.lmao.ninja/v2/countries/Cambodia')
+.then((response) => {
+    return response.json();
+})
+.then((data)=>{
+document.getElementById('cd-Actived').innerHTML = data.active;
+document.getElementById('cd-deaths').innerHTML = data.deaths;
+document.getElementById('cd-recovered').innerHTML = data.recovered;
+document.getElementById('cd-Case').innerHTML = data.cases;
+document.getElementById('cd-tdcases').innerHTML = data.todayCases;
+})
+
